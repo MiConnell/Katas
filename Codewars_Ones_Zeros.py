39d44483d38193681b1d329bc8684e92d99144ec
mikey <connellmp@gmail.com>
2020-08-09 12:25:33 -0700

    Codewars Ones and Zeros Solved

---
+++ Codewars_Ones_Zeros.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/578553c3a1b8d5c40300037c/train/python
+def binary_array_to_number(arr):
+    return int(''.join(map(str, arr)), 2)
+
+
+binary_array_to_number([0, 1, 1, 1])
