8e19f0d346ddc9044262f54d98401d75f2f1f97e
mikey <connellmp@gmail.com>
2020-08-09 12:24:16 -0700

    Codewars Highest and Lowest Solved

---
+++ Codewars_Highest_Lowest.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/554b4ac871d6813a03000035/train/python
+def high_and_low(numbers):
+    numbers = sorted([int(n) for n in numbers.split(' ')])
+    return f'{numbers[-1]} {numbers[0]}'
+
+
+high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")
