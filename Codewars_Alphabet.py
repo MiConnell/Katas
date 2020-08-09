84fd396d1d89967fbe85203c00515b16d262c75d
mikey <connellmp@gmail.com>
2020-08-09 12:48:03 -0700

    Codewars Alphabet Position Solved

---
+++ Codewars_Alphabet.py
@@ -0,0 +1,45 @@
+# https://www.codewars.com/kata/546f922b54af40e1e90001da/train/python
+"""
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+"""
+def alphabet_position(text):
+    alphadict = {
+        'a': 1,
+        'b': 2,
+        'c': 3,
+        'd': 4,
+        'e': 5,
+        'f': 6,
+        'g': 7,
+        'h': 8,
+        'i': 9,
+        'j': 10,
+        'k': 11,
+        'l': 12,
+        'm': 13,
+        'n': 14,
+        'o': 15,
+        'p': 16,
+        'q': 17,
+        'r': 18,
+        's': 19,
+        't': 20,
+        'u': 21,
+        'v': 22,
+        'w': 23,
+        'x': 24,
+        'y': 25,
+        'z': 26
+    }
+
+    return ' '.join([
+        str(alphadict[key]) for t in text.lower()
+        for key in alphadict.keys()
+        if key == t
+    ])
