eb58d003d545fdd2e4ae2b4b0eb7b809db15279e
michael <connellmp@gmail.com>
2020-10-15 16:33:38 -0700

    solve codewars chain

--- codewars_chain.py
+++ codewars_chain.py
@@ -16,10 +16,18 @@ A single call should return the number passed in.
 add(1); // 1
 We should be able to store the returned values and reuse them.
 
-var addTwo = add(2);
+addTwo = add(2);
 addTwo; // 2
 addTwo + 5; // 7
 addTwo(3); // 5
 addTwo(3)(5); // 10
 We can assume any number being passed in will be valid whole number.
 """
+
+
+class CustomInt(int):
+    def __call__(self, v):
+        return CustomInt(self + v)
+
+def add(x):
+    return CustomInt(x)
