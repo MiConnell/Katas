3c21483099daf639b2cd47e13b02013a763862a1
michael <connellmp@gmail.com>
2021-01-12 09:40:27 -0800

    remove unorganized files

--- Codewars_List_Filtering.py
+++
@@ -1,8 +0,0 @@
-# https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/python
-"""
-In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
-"""
-
-
-def filter_list(list):
-    return [l for l in list if type(l) == int]
