00d511401b59eebd86cd701a155365c225007809
michael <connellmp@gmail.com>
2021-02-02 12:08:42 -0800

    solve codewars_spiral

--- codewars/codewars_spiral.py
+++ codewars/codewars_spiral.py
@@ -39,18 +39,19 @@ from typing import List
 
 
 def create_spiral(n: int) -> List[List[int]]:
-    if n < 1 or type(n) is not int:
+    if type(n) is not int or n < 1:
         return []
-    min = n
-    max = n ** 2
-    mid = [0 for _ in range(min)]
-    final = [mid for _ in range(min)]
-    first_ = [i for i in range(1, n + 1)]
-    outer_ = [i for i in range(n + 1, n + n)]
-    final[0] = first_
-    final[-1] = [i for i in range(outer_[-1], outer_[-1] + n)][::-1]
-    return final
+    result = [[0] * n for i in range(n)]
+    vectors_x, vectors_y = [0, 1, 0, -1], [1, 0, -1, 0]
+    curr_x, curr_y, curr_value = 0, -1, 1
+    for i in range(n + n - 1):
+        for _ in range((n + n - i) // 2):
+            curr_x += vectors_x[i % 4]
+            curr_y += vectors_y[i % 4]
+            result[curr_x][curr_y] = curr_value
+            curr_value += 1
+    return result
 
 
 if __name__ == "__main__":
-    print(create_spiral(3))
+    print(create_spiral(5))
