0bca15a5a78d367dc039ffc593bf663a00d0ef29
michael <connellmp@gmail.com>
2021-02-04 15:41:35 -0800

    initial commit

---
+++ codewars/codewars_knight.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/549ee8b47111a81214000941/train/python
+"""
+Given two different positions on a chess board,
+find the least number of moves it would take a knight to get from one to the other.
+The positions will be passed as two arguments in algebraic notation.
+For example, knight("a3", "b5") should return 1.
+
+The knight is not allowed to move off the board. The board is 8x8.
+"""
+
+
+def knight(p1: str, p2: str) -> int:
+    pass
