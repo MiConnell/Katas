6039b9e835e00ef11fdd36631e414f4beae6ef96
michael <connellmp@gmail.com>
2021-02-05 10:26:45 -0800

    implement dict and counter

--- codewars/codewars_unique_string.py
+++ codewars/codewars_unique_string.py
@@ -10,3 +10,22 @@ E.g. string that contains only spaces is like empty string.
 
 It’s guaranteed that array contains more than 3 strings.
 """
+from collections import Counter
+from typing import List
+
+
+def find_uniq(arr: List[str]) -> str:
+    sets = [set(str.lower(s)) for s in arr]
+    dct = {"".join(set(str.lower(a))): a for a in arr}
+    mid_list: List[str] = []
+    for s in sets:
+        mid_list.append(*s)
+    final = dict(Counter("".join(mid_list)))
+    for k, v in final.items():
+        if v == 1:
+            return dct[k]
+    return ""
+
+
+if __name__ == "__main__":
+    print(find_uniq(["Aa", "aaa", "aaaaa", "BbBb", "Aaaa", "AaAaAa", "a"]))
