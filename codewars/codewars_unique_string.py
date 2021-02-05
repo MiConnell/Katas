cc750f4fc9fd0c4391c4ae412e82e4cfa97b03d0
michael <connellmp@gmail.com>
2021-02-05 09:48:23 -0800

    initial commit

---
+++ codewars/codewars_unique_string.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/585d8c8a28bc7403ea0000c3/train/python
+"""
+There is an array of strings. All strings contains similar letters except one. Try to find it!
+
+find_uniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) # => 'BbBb'
+find_uniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) # => 'foo'
+
+Strings may contain spaces. Spaces is not significant, only non-spaces symbols matters.
+E.g. string that contains only spaces is like empty string.
+
+It’s guaranteed that array contains more than 3 strings.
+"""
