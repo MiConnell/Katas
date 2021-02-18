4058075e7c75910c5adbcd103fbe9a21a0d17e92
michael <connellmp@gmail.com>
2021-02-18 11:22:20 -0800

    simplify check logic and prettify print statements

--- codewars/codewars_connect_four.py
+++ codewars/codewars_connect_four.py
@@ -38,34 +38,33 @@ class ConnectFour:
     def __init__(self, moves: List[str], layout: Dict[str, int] = LAYOUT) -> None:
         self.moves = moves
         self.board = [["_"] * 7 for _ in range(6)]
-        self.layout = layout
+        self.layout = layout.copy()
 
     def who_is_winner(self) -> str:
-        self.piece = "Draw"
+        self.draw = "Draw"
+        print("starting board \n")
         print(*self.board, sep="\n")
         for m in self.moves:
             self.col, self.piece = m.split("_")
-            print(f"{self.piece} played in column {self.col}")
+            self.row = self.layout[self.col] + 1
+            print(f"\n{self.piece} played in column {self.col}\n")
             self.board[self.layout[self.col]][COLUMNS[self.col]] = self.piece[0]
-            print(self.layout[self.col], COLUMNS[self.col])
             print(*self.board, sep="\n")
             self.layout[self.col] += 1
             if not self._winning(self.piece[0]) and self.piece != "_":
                 time.sleep(1)
             else:
-                return f"***{self.piece} wins in column {self.col}!***"
-        return self.piece
+                return (
+                    f"\n***{self.piece} wins in column {self.col} row {self.row}!***\n"
+                )
+        return self.draw
 
     def _winning(self, color: str) -> bool:
         self.color = color
         self.board_transposed = list(zip(*self.board))
-        for b in self.board:
-            if self._check_row(b, self.color):
-                return True
-        for b in self.board_transposed:  # type:ignore
-            if self._check_col(b, self.color):  # type:ignore
-                return True
-        return False
+        return any(
+            self._check_col(b, self.color) for b in self.board_transposed  # type:ignore
+        ) or any(self._check_row(b, self.color) for b in self.board)
 
     def _key_func(self, x: Any) -> Any:
         return x[0]
@@ -77,8 +76,8 @@ class ConnectFour:
         self.groups = [list(g) for _, g in groupby(self.b, self._key_func)]
         return any(len(i) == target and i[0] == color for i in self.groups)
 
-    def _check_col(self, b: List[str], color: str, target: int = 4) -> bool:
-        self.b = b
+    def _check_col(self, b: Tuple[str], color: str, target: int = 4) -> bool:
+        self.b = b  # type:ignore
         self.color = color
         self.target = target
         self.groups = [list(g) for _, g in groupby(b, self._key_func)]
@@ -117,7 +116,7 @@ row_check = [
 
 
 if __name__ == "__main__":
-    r = ConnectFour(row_check)
-    print(r.who_is_winner())
     c = ConnectFour(col_check)
     print(c.who_is_winner())
+    r = ConnectFour(row_check)
+    print(r.who_is_winner())
