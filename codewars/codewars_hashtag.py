24a9b1d23e704d70e2bd5b239ec961ae22c5b208
michael <connellmp@gmail.com>
2021-03-25 19:54:32 -0700

    solve hashtag

---
+++ codewars/codewars_hashtag.py
@@ -0,0 +1,38 @@
+# https://www.codewars.com/kata/52449b062fb80683ec000024/train/python
+"""
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+    It must start with a hashtag (#).
+    All words must have their first letter capitalized.
+    If the final result is longer than 140 chars it must return false.
+    If the input or the result is an empty string it must return false.
+
+Examples
+
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+
+"""
+from typing import Union
+
+
+def generate_hashtag(s: str) -> Union[str, bool]:
+    stripped = str.title(s).replace(" ", "")
+    if len(stripped) > 140 or not stripped:
+        return False
+    return f"#{stripped}"
+
+
+examples = [
+    " Hello there thanks for trying my Kata",
+    "    Hello     World   ",
+    "",
+]
+
+if __name__ == "__main__":
+    for e in examples:
+        print(generate_hashtag(e))
